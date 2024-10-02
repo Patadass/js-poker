72c0e5928febf963a726e5a3b120aac5a3906c42
ivanajdinski <ajdinskiivan1@gmail.com>
2024-10-02 22:56:39 +0200

    added class Poker added class Player

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 function random(from,to){
-    return Math.floor(Math.random() * (to - from) + from);
+    return Math.floor(Math.random() * to) + from;
 }
 
 class Card{
@@ -42,13 +42,81 @@ class Deck{
             this.deck[s] = this.deck[i];
             this.deck[i] = ph
         }
-    }//TODO: add this
+    }
+    getTopCard(){
+        return this.deck[this.deck.length-1]
+    }
+    popTopCard(){
+        this.deck.splice(this.deck.length-1,1)
+    }
 }
+
+class Player{
+    constructor(name = "",chips = 0){
+        this.name = name
+        this.chips = chips
+        this.cards = []
+        this.bigBlind = false
+        this.smallBlind = false
+    }
+    addCard(card){
+        this.cards.push(card)
+    }
+    resetCards(){
+        this.cards = []
+    }
+}
+
 class Poker{
     constructor(){
         this.deck = new Deck()
+        this.players = []
+        this.community = []
+    }
+    addPlayer(player){
+        this.players.push(player)
+    }
+    printPlayers(){
+        for(let i in this.players){
+            console.log(this.players[i])
+        }
+    }
+    dealCards(){
+        for(let i in this.players){
+            this.players[i].addCard(this.deck.getTopCard())
+            this.deck.popTopCard()
+        }
+        for(let i in this.players){
+            this.players[i].addCard(this.deck.getTopCard())
+            this.deck.popTopCard()
+        }
+    }
+    dealFlop(){
+        this.deck.popTopCard()
+        let i = 0
+        while(i < 3){
+            this.community.push(this.deck.getTopCard())
+            this.deck.popTopCard()
+            i++
+        }
+    }
+    dealTurn(){
+        this.deck.popTopCard()
+        this.community.push(this.deck.getTopCard())
+        this.deck.popTopCard()
+    }
+    printGame() {
+        for (let i in this.players) {
+            console.log(this.players[i].name, this.players[i].cards[0], this.players[i].cards[1])
+        }
+        for(let i in this.community){
+            console.log(this.community[i].suit, this.community[i].rank)
+        }
     }
 }
-deck = new Deck()
-deck.shuffle()
-deck.printCards()
+game = new Poker()
+game.addPlayer(new Player("Ivan"))
+game.addPlayer(new Player("David"))
+game.deck.shuffle()
+game.dealCards()
+game.printGame()
