14db5fd9c66b315477bac11644384b3adcd9ca68
Patadass <ajdinskiivan1@gmail.com>
2024-10-06 20:15:11 +0200

    added more game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,74 @@
 function init() {
-    const floor = document.createElement("div")
-    floor.id = "floor";
-    document.body.appendChild(floor)
     addPlayer("ivan",100)
     addPlayer("david",100)
+}
+
+function makeInnerCard(rank, suit){
+    let card = document.createElement("div")
+    let innerSuit = document.createElement("div")
+    let img = document.createElement("img")
+    let number = document.createElement("div")
+    card.classList.add("card")
+    innerSuit.classList.add("suit")
+    number.classList.add("number")
+    number.innerHTML = rank
+    if(suit.length > 0){
+        img.src = "../cards/"+suit+".svg"
+    }
+    card.appendChild(innerSuit)
+    innerSuit.appendChild(img)
+    card.appendChild(number)
+    return card
+}
+
+function changeInnerCard(card, rank, suit){
+    let innerSuit = card.getElementsByClassName("suit")[0]
+    innerSuit.firstChild.src = "../cards/"+suit+".svg"
+    card.getElementsByClassName("number")[0].innerHTML = rank
+}
+
+function refreshInnerPlayers(){
+    for(let i in game.players){
+        let innerPlayer = document.getElementsByClassName("player")[i]
+        if(game.players[i].cards[0] === undefined){
+            changeInnerCard(innerPlayer.getElementsByClassName("card")[0], null, "")
+        }else{
+            changeInnerCard(innerPlayer.getElementsByClassName("card")[0], game.players[i].cards[0].rank, game.players[i].cards[0].suit)
+        }
+        if(game.players[i].cards[1] === undefined){
+            changeInnerCard(innerPlayer.getElementsByClassName("card")[1], null, "")
+        }else{
+            changeInnerCard(innerPlayer.getElementsByClassName("card")[1], game.players[i].cards[1].rank, game.players[i].cards[1].suit)
+        }
+        innerPlayer.getElementsByClassName("player-chips")[0].innerHTML = "chips: " + game.players[i].chips
+        innerPlayer.getElementsByClassName("player-bet")[0].innerHTML = "bet: " +  game.players[i].curentBet
+        let name = game.players[i].name
+        if(game.players[i].smallBlindBool){
+            name += " SB"
+        }
+        if(game.players[i].bigBlindBool){
+            name += " BB"
+        }
+        innerPlayer.getElementsByClassName("player-name")[0].innerHTML = name
+        if(game.players[i].action){
+            innerPlayer.getElementsByClassName("player-button-check")[0].disabled = false
+            innerPlayer.getElementsByClassName("player-button-bet")[0].disabled = false
+            innerPlayer.getElementsByClassName("player-button-fold")[0].disabled = false
+            if(game.players[i].curentBet < game.currentBet){
+                innerPlayer.getElementsByClassName("player-button-check")[0].textContent = "Call"
+                innerPlayer.getElementsByClassName("player-button-bet")[0].textContent = "Raise"
+            }
+            if(game.players[i].curentBet === game.currentBet){
+                innerPlayer.getElementsByClassName("player-button-check")[0].textContent = "Check"
+                innerPlayer.getElementsByClassName("player-button-bet")[0].textContent = "Bet"
+            }
+        }
+    }
+}
+
+function refreshInnerPot(){
+    let pot = document.getElementById("pot")
+    pot.getElementsByClassName("pot-chips")[0].innerHTML = game.pot
 }
 
 function addPlayer(name, chips){
@@ -12,82 +77,88 @@ function addPlayer(name, chips){
     let player = document.createElement("div")
     let innerName = document.createElement("div")
     let innerChips = document.createElement("div")
-    let card = document.createElement("div")
-    let suit = document.createElement("div")
-    let img = document.createElement("img")
-    let number = document.createElement("div")
+    let innerBet = document.createElement("div")
     player.classList.add("player")
     innerName.classList.add("player-name")
     innerName.textContent = name
+    if(game.players.at(game.players.length-1).smallBlindBool){
+        innerName.textContent += " SB"
+    }
+    if(game.players.at(game.players.length-1).bigBlindBool){
+        innerName.textContent += " BB"
+    }
     innerChips.classList.add("player-chips")
     innerChips.textContent = "chips: " + chips
-    card.classList.add("card")
-    suit.classList.add("suit")
-    number.classList.add("number")
-    number.innerHTML = "0"
-    img.src = "../cards/club.svg"
+    innerBet.classList.add("player-bet")
+    innerBet.textContent = "bet: " + game.players[game.players.length-1].curentBet
     document.getElementById("floor").appendChild(player)
     player.appendChild(innerName)
     player.appendChild(innerChips)
-    player.appendChild(card)
-    card.appendChild(suit)
-    suit.appendChild(img)
-    card.appendChild(number)
-    card = document.createElement("div")
-    suit = document.createElement("div")
-    img = document.createElement("img")
-    number = document.createElement("div")
-    card.classList.add("card")
-    suit.classList.add("suit")
-    number.classList.add("number")
-    number.innerHTML = "0"
-    img.src = "../cards/club.svg"
-    player.appendChild(card)
-    card.appendChild(suit)
-    suit.appendChild(img)
-    card.appendChild(number)
-
-    let btn = document.createElement("button")
-    btn.textContent = "Check"
-    btn.addEventListener("click", () => {
+    player.appendChild(innerBet)
+    player.appendChild(makeInnerCard(null, ""))
+    player.appendChild(makeInnerCard(null, ""))
+    let buttonCheck = document.createElement("button")
+    buttonCheck.classList.add("player-button-check")
+    buttonCheck.disabled = true
+    buttonCheck.textContent = "Check"
+    buttonCheck.addEventListener("click", () => {
         console.log(game.players[n].name)
     })
-    player.appendChild(btn)
+    player.appendChild(buttonCheck)
+    let buttonBet = document.createElement("button")
+    buttonBet.classList.add("player-button-bet")
+    buttonBet.disabled = true
+    buttonBet.textContent = "Bet"
+    let isRange = false
+    buttonBet.addEventListener("click", () => {
+        if(!isRange){
+            player.removeChild(player.getElementsByClassName("player-button-fold")[0])
+            isRange = true
+            let rangeInput = document.createElement("input")
+            rangeInput.classList.add("player-range-input")
+            rangeInput.type = "range"
+            rangeInput.min = "0"//TODO:Maybe dont allow to bet less than min bet
+            rangeInput.max = game.players[game.players.length-1].chips.toString()
+            rangeInput.step = "1"
+            rangeInput.value = "0"
+            player.appendChild(rangeInput)
+            rangeInput.addEventListener("input", () => {
+                player.getElementsByClassName("player-button-bet")[0].textContent = rangeInput.value
+            })
+            return;
+        }
+        if(isRange){
+            let rangeInput = player.getElementsByClassName("player-range-input")[0]
+            let value = rangeInput.value
+            player.removeChild(player.getElementsByClassName("player-range-input")[0])
+            player.appendChild(buttonFold)
+            if(value > 0){
+                game.bet(parseInt(value))
+            }
+            isRange = false
+        }
+    })
+    player.appendChild(buttonBet)
+    let buttonFold = document.createElement("button")
+    buttonFold.classList.add("player-button-fold")
+    buttonFold.disabled = true
+    buttonFold.textContent = "Fold"
+    buttonFold.addEventListener("click", () => {
+        console.log(game.players[n].name)
+    })
+    player.appendChild(buttonFold)
 }
 
 function displayCommunityFlop(){
     const innerCommunity = document.getElementById("community")
     for(let i in game.community){
-        let card = document.createElement("div")
-        let suit = document.createElement("div")
-        let img = document.createElement("img")
-        let number = document.createElement("div")
-        card.classList.add("card")
-        suit.classList.add("suit")
-        number.classList.add("number")
-        number.innerHTML = game.community[i].getRank()
-        img.src = "../cards/"+game.community[i].getSuit()+".svg"
-        innerCommunity.appendChild(card)
-        card.appendChild(suit)
-        suit.appendChild(img)
-        card.appendChild(number)
+        innerCommunity.appendChild(makeInnerCard(game.community[i].getRank(), game.community[i].getSuit()))
     }
 }
+
 function displayCommunityTurn(i){//i is 3 for Turn and 4 for river
     const innerCommunity = document.getElementById("community")
-    let card = document.createElement("div")
-    let suit = document.createElement("div")
-    let img = document.createElement("img")
-    let number = document.createElement("div")
-    card.classList.add("card")
-    suit.classList.add("suit")
-    number.classList.add("number")
-    number.innerHTML = game.community[i].getRank()
-    img.src = "../cards/"+game.community[i].getSuit()+".svg"
-    innerCommunity.appendChild(card)
-    card.appendChild(suit)
-    suit.appendChild(img)
-    card.appendChild(number)
+    innerCommunity.appendChild(makeInnerCard(game.community[i].getRank(),game.community[i].getSuit()))
 }
 
 function flush(){
@@ -118,6 +189,7 @@ class Deck{
         this.makeCards()
     }
     makeCards(){
+        this.deck = []
         let suit = ["heart","diamond","club","spade"]
         let j = 1
         let i = 0
@@ -158,8 +230,10 @@ class Player{
         this.name = name
         this.chips = chips
         this.cards = []
-        this.bigBlind = false
-        this.smallBlind = false
+        this.bigBlindBool = false
+        this.smallBlindBool = false
+        this.action = false
+        this.curentBet = 0
     }
     addCard(card){
         this.cards.push(card)
@@ -168,12 +242,12 @@ class Player{
         this.cards = []
     }
     makeBB(){
-        this.bigBlind = true
-        this.smallBlind = false
+        this.bigBlindBool = true
+        this.smallBlindBool = false
     }
     makeSB(){
-        this.smallBlind = true
-        this.bigBlind = false
+        this.smallBlindBool = true
+        this.bigBlindBool = false
     }
 }
 
@@ -188,6 +262,12 @@ class Poker{
         this.position = 0
     }
     addPlayer(player){
+        if(this.players.length === 0){
+            player.smallBlindBool = true
+        }
+        if(this.players.length === 1){
+            player.bigBlindBool = true
+        }
         this.players.push(player)
     }
     printPlayers(){
@@ -195,14 +275,40 @@ class Poker{
             console.log(this.players[i])
         }
     }
-    dealCards(){
-        for(let i in this.players){
-            this.players[i].addCard(this.deck.getTopCard())
-            this.deck.popTopCard()
+    nextPosition(){
+        if(this.position >= this.players.length-1){
+            this.position = 0;
+        }else{
+            this.position++;
         }
+    }
+    prevPosition(){
+        if(this.position < 0){
+            this.position = this.players.length - 1;
+        }else{
+            this.position--;
+        }
+    }
+    dealCards(startPos){
+        let phPos = startPos
         for(let i in this.players){
-            this.players[i].addCard(this.deck.getTopCard())
+            if(phPos >= this.players.length){
+                phPos = 0
+            }
+            this.players[phPos].addCard(this.deck.getTopCard())
             this.deck.popTopCard()
+            refreshInnerPlayers()
+            phPos++
+        }
+        phPos = startPos
+        for(let i in this.players){
+            if(phPos >= this.players.length){
+                phPos = 0
+            }
+            this.players[phPos].addCard(this.deck.getTopCard())
+            this.deck.popTopCard()
+            refreshInnerPlayers()
+            phPos++
         }
     }
 
@@ -213,6 +319,9 @@ class Poker{
         this.players[this.position].chips -= bet
         this.pot += bet
         this.currentBet = bet
+        this.players[this.position].curentBet += bet
+        refreshInnerPlayers()
+        refreshInnerPot()
         return true
     }
     raise(bet){
@@ -222,10 +331,30 @@ class Poker{
         this.players[this.position].chip -= bet
         this.pot += bet
         this.currentBet = bet
+        this.players[this.position].curentBet = bet
         return true
     }
-    nextDeal(){
+    call(){
+        this.players[this.position].chips -= this.currentBet - this.players[this.position].curentBet
+        this.pot += this.currentBet - this.players[this.position].curentBet
+        this.players[this.position].curentBet = this.currentBet - this.players[this.position].curentBet
+    }
 
+    nextDeal(){
+        this.deck.makeCards()
+        this.deck.shuffle()
+        let startPos = 0
+        while(!this.players[startPos].smallBlindBool){
+            startPos++
+        }
+        this.position = startPos
+        this.dealCards(this.position)
+        this.bet(this.minBet)
+        this.nextPosition()
+        this.bet(2*this.minBet)
+        this.position = startPos
+        this.players[this.position].action = true
+        refreshInnerPlayers()
     }
     dealCommunityFlop(){
         this.deck.popTopCard()
@@ -260,10 +389,6 @@ class Poker{
     }
 }
 game = new Poker()
-//game.addPlayer(new Player("Ivan",100))
-//game.addPlayer(new Player("David",100))
-game.deck.shuffle()
-game.dealCards()
-game.printGame()
+
 
 
